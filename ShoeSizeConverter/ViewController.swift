7733d7071da25eb872e28b21325f5db10571e51a
Carmen Albu <carmenelena.albu@gmail.com>
2014-12-30 17:13:27 +0200

    Initial Commit

---
+++ ShoeSizeConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoeSizeConverter
+//
+//  Created by Carmen Albu on 30/12/14.
+//  Copyright (c) 2014 Carmen Albu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
